6f6d92f404e3c9c701131316a7cdd40c74aa20c5
MavizZafar <maviz.zafar@nxb.com.pk>
2013-09-23 21:14:02 +0500

    initial cimmit ..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '3389aea7a6bf4496e9ee1422c4d706a9fa49240e8d068abd02048467bd737e9f19b6ee0d0fb2159086adecc60e382d72be3d00d781f7e8c5ddefa9ee735f4d51'
